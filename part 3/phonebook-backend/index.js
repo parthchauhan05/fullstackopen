0c56f0559ba4052d503c7b53f61a302e8770d1b3
Parth Chauhan <webdevservice.parth@gmail.com>
2025-07-15 16:26:54 -0400

    part 3.1 - 3.8

---
+++ part 3/phonebook-backend/index.js
@@ -0,0 +1,79 @@
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+morgan.token('body', (request, response) => {
+    return JSON.stringify(request.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+const persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+const generateId = () => {
+  return String(parseInt(Math.random() * 100000000000))
+}
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    response.send(`<p>Phonebook has info for ${persons.length} people.<br/>${new Date()}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.filter(item => item.id == id)
+    if (person.length) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(item => item.id !== id)
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const person = request.body
+    if (!person.name || !person.number) {
+        return response.status(400).json({error: "Name or number is missing"})
+    }
+    const duplicatePerson = persons.find(item => item.name == person.name)
+    if (duplicatePerson) {
+        return response.json({ error: 'name must be unique' })
+    }
+    person.id = generateId()
+    persons.concat(person)
+    response.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
